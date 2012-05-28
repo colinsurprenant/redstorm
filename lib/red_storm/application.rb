d07a8d0066acddcd184eb0a6faa570fbc45d41db
Colin Surprenant <colin.surprenant@gmail.com>
2012-05-28 16:03:06 -0400

    support gems command

--- lib/red_storm/application.rb
+++ lib/red_storm/application.rb
@@ -4,13 +4,13 @@ module RedStorm
     TASKS_FILE = "#{RedStorm::REDSTORM_HOME}/lib/tasks/red_storm.rake" 
 
     def usage
-      puts("usage: redstorm install|examples|jar <project_directory>|local <topology_class_file>")
+      puts("usage: redstorm install|gems|examples|jar <project_directory>|local <topology_class_file>")
       exit(1)
     end
 
     def run(args)
       if args.size > 0
-        if ["install", "examples", "jar"].include?(args[0])
+        if ["install", "examples", "jar", "gems"].include?(args[0])
           load(TASKS_FILE)
           Rake::Task[args.shift].invoke(*args)
           exit

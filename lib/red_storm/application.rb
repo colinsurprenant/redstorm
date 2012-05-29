a646ba735179d78fd88ad87a16d87f7ed530305a
Colin Surprenant <colin.surprenant@gmail.com>
2012-05-29 14:03:47 -0400

    suppport --1.8 and --1.9 switch in bin/redstorm and set runtime env accordingly

--- lib/red_storm/application.rb
+++ lib/red_storm/application.rb
@@ -1,4 +1,7 @@
 module RedStorm
+
+  DEFAULT_RUBY_VERSION = "--1.8"
+  RUNTIME = {}
   
   class Application 
     TASKS_FILE = "#{RedStorm::REDSTORM_HOME}/lib/tasks/red_storm.rake" 
@@ -10,17 +13,19 @@ module RedStorm
 
     def run(args)
       if args.size > 0
-        if ["install", "examples", "jar", "gems"].include?(args[0])
+        version = args.delete("--1.8") || args.delete("--1.9") || DEFAULT_RUBY_VERSION
+        RUNTIME['RUBY_VERSION'] = version
+
+        if ["install", "examples", "jar", "gems", "deps", "build"].include?(args[0])
           load(TASKS_FILE)
           Rake::Task[args.shift].invoke(*args)
           exit
         elsif args.size >= 2 && args.include?("local") 
           args.delete("local")
-          version = args.delete("--1.8") || args.delete("--1.9") || "--1.8"
           if args.size == 1
             file = args[0]
             load(TASKS_FILE)
-            Rake::Task['launch'].invoke("local", version, file)
+            Rake::Task['launch'].invoke("local", file)
             exit
           end
         end

06c9155a0f50860f38a2f665d862be91c6e8c1c5
Colin Surprenant <colin.surprenant@gmail.com>
2012-06-27 10:25:16 -0400

    missing java require and changed java object calling namespace

--- lib/red_storm/simple_topology.rb
+++ lib/red_storm/simple_topology.rb
@@ -1,3 +1,4 @@
+require 'java'
 require 'red_storm/configuration'
 require 'red_storm/configurator'
 
@@ -111,11 +112,11 @@ module RedStorm
 
       builder = TopologyBuilder.new
       self.class.spouts.each do |spout|
-        declarer = builder.setSpout(spout.id, spout.new_instance(base_class_path), java.lang.Integer.new(spout.parallelism))
+        declarer = builder.setSpout(spout.id, spout.new_instance(base_class_path), Java::JavaLang::Integer.new(spout.parallelism))
         declarer.addConfigurations(spout.config)
       end
       self.class.bolts.each do |bolt|
-        declarer = builder.setBolt(bolt.id, bolt.new_instance(base_class_path), java.lang.Integer.new(bolt.parallelism))
+        declarer = builder.setBolt(bolt.id, bolt.new_instance(base_class_path), Java::JavaLang::Integer.new(bolt.parallelism))
         declarer.addConfigurations(bolt.config)
         bolt.define_grouping(declarer)
       end

4b2080ce59557dc2eb311b9e1dd646ec6bde7cf3
Colin Surprenant <colin.surprenant@gmail.com>
2012-10-19 17:54:15 -0400

    instructions for Dependencies

--- examples/simple/kafka_topology.rb
+++ examples/simple/kafka_topology.rb
@@ -4,6 +4,23 @@ java_import 'storm.kafka.SpoutConfig'
 java_import 'storm.kafka.StringScheme'
 java_import 'storm.kafka.KafkaSpout'
 
+# the KafkaTopology obviously requires a Kafka server running, you can ajust the
+# host and port below. 
+#
+# custom dependencies are required for the Kafka and Scala jars. put the following
+# dependencies in the "Dependencies" file in the root of your RedStorm project:
+#
+# {
+#   :storm_artifacts => [
+#     "storm:storm:0.8.1, transitive=true",
+#   ],
+#   :topology_artifacts => [
+#     "org.jruby:jruby-complete:1.6.8, transitive=false",
+#     "org.scala-lang:scala-library:2.8.0, transitive=false",
+#     "storm:kafka:0.7.0-incubating, transitive=false",
+#     "storm:storm-kafka:0.8.0-wip4, transitive=false",
+#   ],
+# }
 
 class KafkaTopology < RedStorm::SimpleTopology
   spout_config = SpoutConfig.new(

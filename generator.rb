af0948a46e70ee04a6a2d23b39b4a3de7abaeafa
Paul Bergeron <paul.d.bergeron@gmail.com>
2012-12-11 21:41:27 -0800

    Further updates to proxy class generator

--- generator.rb
+++ generator.rb
@@ -6,20 +6,30 @@ require 'active_support/core_ext'
 
 Dir["../triggit-storm/target/dependency/storm/default/*"].each{|f| $CLASSPATH << File.expand_path(f) }
 
-to_generate = ["storm.trident.spout.IBatchSpout"]
-
 
 PROXY_JRUBY_TEMPLATE = File.read("./ruby_proxy.erb")
 PROXY_JAVA_TEMPLATE = File.read("./java_proxy.erb")
 
-to_generate = ["storm.trident.spout.IBatchSpout"]
+to_generate = ["storm.trident.operation.Function"]
 
+
+# Return all java functions of a java class
+def get_functions(jlass)
+  jlass.declared_instance_methods.concat( jlass.interfaces.map{|i| get_functions(i) }.flatten )
+end
+
+# Return all java deps of a class
+def get_java_deps(functions, klass)
+  functions.map{|f| [f.argument_types.map{|at| at.name}, f.return_type ? f.return_type.name : "void"] }.flatten.uniq.reject{|t| t.split('.').count == 1} << klass
+end
 
 to_generate.each do |klass|
-  _functions = Object.const_get(java_import(klass)[0].to_s.split("::")[-1]).java_class.declared_instance_methods
+  _functions = get_functions(Object.const_get(java_import(klass)[0].to_s.split("::")[-1]).java_class)
 
-  java_deps = _functions.map{|f| [f.argument_types.map{|at| at.name}, f.return_type ? f.return_type.name : "void"] }.flatten.uniq.reject{|t| t.split('.').count == 1} << klass
+  java_deps = get_java_deps(_functions, klass)
 
+
+  # Boil down functions to {:function_name => {:return_type => type, :args => {:arg_var_name => :arg_var_type, ...} } }
   functions = _functions.reduce({}) do |memo, f|
     memo[:"#{f.name}"] = {
       :return_type => f.return_type ? f.return_type.name.split('.')[-1] : "void",
@@ -30,10 +40,12 @@ to_generate.each do |klass|
 
   interface_name = klass.split(".")[-1]
 
-  ruby_class_name = interface_name[1..-1]
+  # IBlah to Blah if IBlah
+  ruby_class_name = interface_name.starts_with?('I') ? interface_name[1..-1] : interface_name
 
   java_class_name = "JRuby#{ruby_class_name}"
 
+  # Rubyify java functions into {:method_name => {:return_type => type, :args => {:arg_var_name => :arg_var_type, ...} } }
   methods = functions.map do |f_name, params|
     {f_name.to_s.underscore.to_sym => {:return_type => params[:return_type], :args => params[:args].map{|name, type| {name.to_s.underscore.to_sym => type}}.reduce({}){|m,o| m.merge(o)} }}
   end.reduce({}){|m,o| m.merge(o)}

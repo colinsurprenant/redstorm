ad2dd0f201e022146f5ef289c8a4f7d50ea1dc1b
Colin Surprenant <colin.surprenant@gmail.com>
2013-05-09 15:55:55 -0400

    accept dots for camelcasing

--- lib/red_storm/configurator.rb
+++ lib/red_storm/configurator.rb
@@ -20,7 +20,7 @@ module RedStorm
     private
 
     def self.camel_case(s)
-      s.to_s.gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase }
+      s.to_s.gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_|\.)(.)/) { $1.upcase }
     end
   end
 end

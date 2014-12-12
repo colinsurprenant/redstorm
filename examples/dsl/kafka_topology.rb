6d0c29b8e86a2ddcc91da0f391026745b7eb1711
JasonK <jasonk@rccb.osis.gov>
2014-12-12 15:13:00 +0000

    Updates Storm to 0.9.3 and uses the official storm-kafka spout for the kafka example. Also fixes a small bug related to ~ when using vagrant to run a local cluster.

--- examples/dsl/kafka_topology.rb
+++ examples/dsl/kafka_topology.rb
@@ -12,10 +12,11 @@ require 'red_storm'
 # dependencies in the "ivy/topology_dependencies.xml" file in the root of your RedStorm project
 # (in addition to the default dependencies, and any additions you may have added):
 
-# <dependency org="org.scala-lang" name="scala-library" rev="2.9.2" conf="default" transitive="false"/>
-# <dependency org="org.apache.kafka" name="kafka_2.9.2" rev="0.8.1.1" conf="default" transitive="false" />
-# <dependency org="net.wurstmeister.storm" name="storm-kafka-0.8-plus" rev="0.4.0" conf="default" transitive="false" />
+# <dependency org="org.scala-lang" name="scala-library" rev="2.10.1" conf="default" transitive="false"/>
+# <dependency org="org.apache.kafka" name="kafka_2.10" rev="0.8.1.1" conf="default" transitive="false" />
+# <dependency org="org.apache.storm" name="storm-kafka" rev="0.9.3" conf="default" transitive="true" />
 # <dependency org="com.yammer.metrics" name="metrics-core" rev="2.2.0"/>
+# <exclude org="org.slf4j" module="slf4j-log4j12"/>
 
 class SplitStringBolt < RedStorm::DSL::Bolt
   on_receive {|tuple| String.from_java_bytes(tuple.value(:bytes)).split.map{|w| [w]}}
